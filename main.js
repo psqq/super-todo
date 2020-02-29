69c34e85d2ff35f3d6d2c432e645260d9b7ed71e
Sergey Pestov <pestov-sa@yandex.ru>
2020-02-29 09:02:21 +0300

    main menu, list of files

--- main.js
+++ main.js
@@ -1,4 +1,41 @@
 
+class List {
+  items = [];
+  add(item) {
+    this.items.push(item);
+  }
+  render(el) {
+    let s = '';
+    for(let item of this.items) {
+      s += `<li>${item}</li>`;
+    }
+    s = `<ul>${s}</ul>`;
+    el.innerHTML = s;
+  }
+}
+
 function showListOfFiles(el) {
   if (!el) el = document.body;
+  let i = 0;
+  let list = new List();
+  while (1) {
+    let key  = localStorage.key(i);
+    if (!key) {
+      break;
+    }
+    list.add(key);
+    i++;
+  }
+  list.add(`<button onclick="showMainMenu()">Show main menu</button>`);
+  list.render(el);
 }
+
+
+function showMainMenu(el) {
+  if (!el) el = document.body;
+  let list = new List();
+  list.add(`<button onclick="showListOfFiles()">Show list of files</button>`);
+  list.render(el);
+}
+
+showMainMenu();
